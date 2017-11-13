c8e7e44e47539b356064df4fa507acf6b18316fe
cliff538 <cliff538@yahoo.com>
2017-11-13 13:23:14 -0700

    Addes UIAlert.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -6,7 +6,8 @@
 //  Copyright (c) 2015 London App Brewery. All rights reserved.
 //
 
-// Check out info.plist. To get status bar to "light" check the box under General and modify plist. 2 steps to make it work. 
+// Check out info.plist. To get status bar to "light" check the box under General and modify plist. 2 steps to make it work.
+// Had to use Menu -> Product -> Clean to get code to work on the closure under nextQuestion() UIAlertAction. 
 
 import UIKit
 
@@ -29,9 +30,13 @@ class ViewController: UIViewController {
 		// MARK: - View Did Load
 		override func viewDidLoad() {
         super.viewDidLoad()
-				//when the app starts it loads up the first question, sends questionText to questionLabel.
-				let firstQuestion = allQuestions.list[0]
-					questionLabel.text = firstQuestion.questionText
+			
+				nextQuestion() //clean and simple. see below for long handed way.
+			
+			//when the app starts it loads up the first question, sends questionText to questionLabel.
+					//this is one way to do it. but, basically this is the nextQuestion function.
+					/* let firstQuestion = allQuestions.list[0]
+					questionLabel.text = firstQuestion.questionText */
 		
 		
         
@@ -39,7 +44,7 @@ class ViewController: UIViewController {
 
 
 		// MARK: - Actions
-		@IBAction func answerPressed(_ sender: AnyObject) { //attatched to two buttons in UI, "True" and "False".
+		@IBAction func answerPressed(_ sender: AnyObject) { //attatched to two buttons in UI, "True" with tag1 and "False" with tag2.
 			if sender.tag == 1 {
 				pickedAnswer = true
 			} else if sender.tag == 2 {
@@ -48,8 +53,8 @@ class ViewController: UIViewController {
 			
 			checkAnswer()
 			questionNumber += 1
-			//questionNumber is incremented and is used to grab the next question from array.
-			questionLabel.text = allQuestions.list[questionNumber].questionText
+			nextQuestion()
+			
 			
   
     }
@@ -62,6 +67,21 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
+			
+			if questionNumber <= 12 {
+			//questionNumber is incremented and is used to grab the next question from array.
+			questionLabel.text = allQuestions.list[questionNumber].questionText
+			} else {
+				let alert = UIAlertController(title: "Awesome", message: "You've finished all the questions, do you want to try again?", preferredStyle: .alert)
+				
+				let restartAction = UIAlertAction(title: "Restart", style: .default, handler:
+				{ (UIAlertAction) in
+					self.startOver() //this is a closure. You have to use self here before the function. the "in" tips you off. You are "in" a closure.
+				})
+				
+				alert.addAction(restartAction)
+				present(alert, animated: true, completion: nil)
+			}
         
     }
     
@@ -79,6 +99,9 @@ class ViewController: UIViewController {
     
     
     func startOver() {
+			
+			questionNumber = 0
+			nextQuestion()
        
     }
 	
